d80ddb17aa91775edbfc3eba97fa32b86a40cd66
imp <imp.sergei@gmail.com>
2021-06-30 11:56:57 +0300

    62. Spring MVC форма input

--- src/main/java/com/tregulov/spring/mvc/MyController.java
+++ src/main/java/com/tregulov/spring/mvc/MyController.java
@@ -2,6 +2,7 @@ package com.tregulov.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -16,26 +17,15 @@ public class MyController {
     }
 
     @RequestMapping("/askDetails")
-    public String askEmplyeeDetails() {
+    public String askEmplyeeDetails(Model model) {
+        model.addAttribute("employee",new Employee());
+
         return "ask-emp-details-view";
     }
 
-    //    @RequestMapping("/showDetails")
-//    public String showEmpDetails() {
-//        return "show-emp-details-view";
-//    }
-//    @RequestMapping("/showDetails")
-//    public String showEmpDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. "+ empName;
-//        model.addAttribute("nameAttribute", empName);
-//        return "show-emp-details-view";
-//    }
-
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
-        empName = "Mr. "+ empName;
-        model.addAttribute("nameAttribute", empName);
+    public String showEmpDetails(@ModelAttribute("employee") Employee emp) {
+        emp.setName("Mr. "+emp.getName());
         return "show-emp-details-view";
     }
 
--- src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -13,6 +13,8 @@
 <br>
 <br>
 <%--Your name: ${param.employeeName}--%>
-Your name: ${nameAttribute}
+Your name: ${employee.name}<br>
+Your surname: ${employee.surname}<br>
+Your salary: ${employee.salary}<br>
 </body>
 </html>
